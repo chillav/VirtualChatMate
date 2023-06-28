8e3669d4332ea16283c7e03de86e2d8cac0405ca
Vika Krasovitova <vika.krasovotova@gmail.com>
2023-06-28 21:19:39 +0400

    dependency connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,15 @@
 plugins {
     id("com.android.application") version "8.1.0-rc01" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("io.realm.kotlin") version "1.8.0" apply false
+
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("io.realm:realm-gradle-plugin:10.15.1")
+    }
 }
