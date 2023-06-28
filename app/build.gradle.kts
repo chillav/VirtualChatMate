2095bc0c8ed912f7081f24a1a4589e874ed7b982
Vika Krasovitova <vika.krasovotova@gmail.com>
2023-06-28 21:46:59 +0400

    added koin and corrective java compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -62,7 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    // TODO    implementation("io.insert-koin:koin-androidx-compose:1.0.3")
+    implementation("io.insert-koin:koin-androidx-compose:3.4.5")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
