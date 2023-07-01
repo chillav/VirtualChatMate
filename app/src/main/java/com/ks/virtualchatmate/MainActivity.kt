ae0b0a9d662f584d7073e5c9d6ff4690afe6d88d
ArmenianboyKover <dreamyinpolar@gmail.com>
2023-07-01 10:29:52 +0300

    added Chat input field

--- app/src/main/java/com/ks/virtualchatmate/MainActivity.kt
+++ app/src/main/java/com/ks/virtualchatmate/MainActivity.kt
@@ -3,12 +3,15 @@ package com.ks.virtualchatmate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.ks.virtualchatmate.chat.ChatInputField
 import com.ks.virtualchatmate.chat.Toolbar
 import com.ks.virtualchatmate.ui.theme.VirtualChatMateTheme
 
@@ -23,10 +26,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     topBar = {
                         Toolbar()
+                    }, bottomBar = {
+                        ChatInputField(
+                            modifier = Modifier
+                                .wrapContentHeight()
+                                .fillMaxWidth()
+                        )
                     }
 
                 ) { contentPadding ->
-                    Box(modifier = Modifier.padding(contentPadding))
+                    Column(
+                        modifier = Modifier
+                            .padding(contentPadding)
+                            .fillMaxSize()
+                    ) {
+
+                    }
                 }
             }
         }
--- app/src/main/java/com/ks/virtualchatmate/chat/ChatScreen.kt
+++ app/src/main/java/com/ks/virtualchatmate/chat/ChatScreen.kt
@@ -2,15 +2,25 @@ package com.ks.virtualchatmate.chat
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -18,12 +28,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ks.virtualchatmate.R
 import com.ks.virtualchatmate.ui.theme.ArrowBackColor
+import com.ks.virtualchatmate.ui.theme.BottomIconColor
 
-@Preview
 @Composable
 fun Toolbar() {
     Row(
@@ -41,7 +50,7 @@ fun Toolbar() {
                 .align(Alignment.CenterVertically)
         )
         Image(
-            painter = painterResource(id = R.drawable.perfil),
+            painter = painterResource(id = R.drawable.ic_avatar),
             contentDescription = null,
             modifier = Modifier
                 .size(33.dp)
@@ -59,6 +68,37 @@ fun Toolbar() {
                 .padding(start = 8.dp)
                 .align(Alignment.CenterVertically)
 
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChatInputField(modifier: Modifier = Modifier) {
+    var messageText by remember { mutableStateOf("") }
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        TextField(
+            value = messageText,
+            onValueChange = { messageText = it },
+            modifier = Modifier
+                .padding(start = 20.dp, end = 20.dp, bottom = 16.dp)
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .clip(CircleShape),
+            singleLine = false,
+            placeholder = { Text(text = "Ввидите сообщение") },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.Send,
+                    tint = BottomIconColor,
+                    modifier = Modifier,
+                    contentDescription = null
+                )
+            }
         )
     }
 }
