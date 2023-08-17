8f134111b2b6bdbec67cbae3e393cfa41ee4337e
Vika Krasovitova <vika.krasovotova@gmail.com>
2023-08-17 13:50:49 +0400

    if user already authorization then first screen with Chat

--- app/src/main/java/com/ks/virtualchatmate/MainActivity.kt
+++ app/src/main/java/com/ks/virtualchatmate/MainActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.ks.virtualchatmate.authorization.AuthorizationScreen
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ks.virtualchatmate.navigation.SetupNavigationGraph
 import com.ks.virtualchatmate.ui.theme.VirtualChatMateTheme
 
@@ -18,7 +16,10 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             VirtualChatMateTheme {
-                SetupNavigationGraph(navController = rememberNavController())
+                SetupNavigationGraph(
+                    navController = rememberNavController(),
+                    user = Firebase.auth.currentUser
+                )
             }
         }
     }
--- app/src/main/java/com/ks/virtualchatmate/navigation/NavGraph.kt
+++ app/src/main/java/com/ks/virtualchatmate/navigation/NavGraph.kt
@@ -4,17 +4,23 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.google.firebase.auth.FirebaseUser
 import com.ks.virtualchatmate.authorization.AuthorizationScreen
 import com.ks.virtualchatmate.chat.ChatScreen
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun SetupNavigationGraph(
-    navController: NavHostController
+    navController: NavHostController,
+    user: FirebaseUser?
 ) {
     NavHost(
         navController = navController,
-        startDestination = Screen.Authorization.route
+        startDestination = if (user != null) {
+            Screen.Chat.route
+        } else {
+            Screen.Authorization.route
+        }
     ) {
         composable(
             route = Screen.Authorization.route
